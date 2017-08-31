70a3d6e94d11f0dca08c177c3feb6ba56571eff8
Michael Davidsaver <mdavidsaver@gmail.com>
2017-08-30 19:24:48 -0500

    mis-merge makeTestfile.pl

--- src/tools/makeTestfile.pl
+++ src/tools/makeTestfile.pl
@@ -59,14 +59,8 @@ use Cwd 'abs_path';
 \$ENV{HARNESS_ACTIVE} = 1 if scalar \@ARGV && shift eq '-tap';
 \$ENV{TOP} = abs_path(\$ENV{TOP}) if exists \$ENV{TOP};
 
-if (\$^O eq 'MSWin32') {
-    # Use system on Windows, exec doesn't work the same there and
-    # GNUmake thinks the test has finished as soon as Perl exits.
-    system('./$exe') == 0 or die "Can't run $exe: \$!\\n";
-}
-else {
-    exec './$exe' or die "Can't run $exe: \$!\\n";
-}
+system('$exec') == 0 or die "Can't run $exec: \$!\\n";
+
 EOF
 
 close $OUT or die "Can't close $target: $!\n";
