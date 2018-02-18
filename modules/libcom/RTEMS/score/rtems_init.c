b3c583197fc836d7bf735c57c627cfc97b6be669
Michael Davidsaver <mdavidsaver@gmail.com>
2018-02-18 12:53:06 -0800

    RTEMS: e500 idle power management for QEMU
    Avoid wasting host CPU, and battery, when
    RTEMS guest is idling.

--- modules/libcom/RTEMS/score/rtems_init.c
+++ modules/libcom/RTEMS/score/rtems_init.c
@@ -515,6 +515,8 @@ static void zonesetCallFunc(const iocshArgBuf *args)
 }
 
 
+// see qemu_idle.c
+extern void qemu_idle_register(void);
 /*
  * Register RTEMS-specific commands
  */
@@ -526,6 +528,7 @@ static void iocshRegisterRTEMS (void)
     iocshRegister(&nfsMountFuncDef, nfsMountCallFunc);
 #endif
     iocshRegister(&zonesetFuncDef, &zonesetCallFunc);
+    qemu_idle_register();
 }
 
 /*
