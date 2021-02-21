80b1960671d98dc50a2c666a0c57589c0e552a3a
Michael Davidsaver <mdavidsaver@gmail.com>
2021-02-21 13:03:04 -0800

    Add DB link modifier
    Marks links which should not be CA_LINK.
    Initially, only warn if not satisfied.

--- modules/database/src/ioc/dbStatic/dbStaticLib.c
+++ modules/database/src/ioc/dbStatic/dbStaticLib.c
@@ -1943,11 +1943,12 @@ char * dbGetString(DBENTRY *pdbentry)
             else if(pvlMask&pvlOptCP) ppind=3;
             else if(pvlMask&pvlOptCPP) ppind=4;
             else ppind=0;
-            dbMsgPrint(pdbentry, "%s%s%s%s",
+            dbMsgPrint(pdbentry, "%s%s%s%s%s",
                    plink->value.pv_link.pvname ? plink->value.pv_link.pvname : "",
                    (plink->flags & DBLINK_FLAG_TSELisTIME) ? ".TIME" : "",
                    ppstring[ppind],
-                   msstring[plink->value.pv_link.pvlMask&pvlOptMsMode]);
+                   msstring[plink->value.pv_link.pvlMask&pvlOptMsMode],
+                   pvlMask & pvlOptDB ? " DB" : "");
             break;
         }
         case VME_IO:
@@ -2039,9 +2040,10 @@ char * dbGetString(DBENTRY *pdbentry)
                 pvlMask = plink->value.pv_link.pvlMask;
                 if (pvlMask&pvlOptCA) ppind=2;
                 else ppind=0;
-                dbMsgPrint(pdbentry, "%s%s",
+                dbMsgPrint(pdbentry, "%s%s%s",
                     plink->value.pv_link.pvname ? plink->value.pv_link.pvname : "",
-                    ppind ? ppstring[ppind] : "");
+                    ppind ? ppstring[ppind] : "",
+                    pvlMask & pvlOptDB ? " DB" : "");
                 break;
             }
             default :
@@ -2374,6 +2376,11 @@ long dbParseLink(const char *str, short ftype, dbLinkInfo *pinfo)
         else if (strstr(pstr, "MSI")) pinfo->modifiers |= pvlOptMSI;
         else if (strstr(pstr, "MSS")) pinfo->modifiers |= pvlOptMSS;
         else if (strstr(pstr, "MS")) pinfo->modifiers |= pvlOptMS;
+
+        if (strstr(pstr, "DB")) {
+            pinfo->modifiers &= ~(pvlOptCA | pvlOptCP | pvlOptCPP); /* pvlOptPP is ok */
+            pinfo->modifiers |= pvlOptDB;
+        }
 
         /* filter modifiers based on link type */
         switch(ftype) {
